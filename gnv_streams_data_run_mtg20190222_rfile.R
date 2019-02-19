5716af2c3077730cf8da6e73a8e6056a971cd88d
ETaylor21 <etaylor21@ufl.edu>
2019-02-19 18:28:59 -0500

    20190219 - commit from RStudio Project parameter descriptions

---
+++ gnv_streams_data_run_mtg20190222_rfile.R
@@ -0,0 +1,19 @@
+##Data run for Lab Meeting on 2/22/2019
+
+##Parameter List with Units
+#DO - dissolved oxygen (mg/L)
+#DO_Sat - dissolved oxygen saturation (%)
+#Temp - water temperature (deg C)
+#SpCond - specific conductivity (uS/cm)
+#pH - pH (SU)
+#Turb - turbidity (NTU)
+
+####NOx, O_P, NH4 all filtered samples###
+
+#NOx - Nitrate(NO3)-Nitrite(NO2)-N (mg/L)
+#OP - orthophosphate(P) (mg/L)
+#NH4 - ammonia(N) (mg/L)
+
+################################################################
+
+#set working directory
