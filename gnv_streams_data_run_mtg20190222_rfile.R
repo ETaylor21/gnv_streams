ee720b01f5fc6ffcb4e137e6bdacf10802daa27b
ETaylor21 <etaylor21@ufl.edu>
2019-02-20 10:33:40 -0500

    notes on how to change data spread. I think it will be easily to separate nutrient data and field parameters bc nutrient data has reps and field parameters don't.
    I initially had data with all parameters in a column but now i've made each parameter a column. May change this but I want to see for now bc there was just a ton of redundant info.
    
    I made two csv files now, one for each, but I want to save this code for reference on how to change spread and save to a csv

--- gnv_streams_data_run_mtg20190222_rfile.R
+++ gnv_streams_data_run_mtg20190222_rfile.R
@@ -14,6 +14,32 @@
 #OP - orthophosphate(P) (mg/L)
 #NH4 - ammonia(N) (mg/L)
 
+####Sample Replications (col = Rep) 
+#a = 1; b = 2; c = 3; d = not a rep just a reading
+
 ################################################################
 
+#call in packages
+
+library(tidyverse)
+
+
 #set working directory
+
+setwd('C:/Users/etaylor21/Documents/gnv_streams')
+
+#call in data file
+
+data=read_csv('gnv_streams_data_run_mtg20190222.csv')
+
+#reshape data to organize
+
+data_spread=spread(data = data, key = 'Parameter', value = 'Result')
+as.data.frame(data_spread)
+
+#create new csv with spread data
+
+write_excel_csv(data_spread, path = 'C:/Users/etaylor21/Documents/gnv_streams/gnv_streams_data_run_mtg20190222.csv', 
+                na = 'NA', append =FALSE, col_names = TRUE)
+
+
