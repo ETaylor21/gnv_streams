36ec63829c482efcab4ef62e9d52ea14a368c785
ETaylor21 <etaylor21@ufl.edu>
2019-02-21 10:52:11 -0500

    tryng to make a scatterplot but the days are stacking and I'm not sure why. I've tried a barchart too and it looks even worse. Also have tried doing geo_jitter which helps a little but not by much. I read you can try doing x=factor(Date) and that would remove spaces in between dates which helped for the bar chart but not for the scatter plot.

--- gnv_streams_data_run_mtg20190222_rfile.R
+++ gnv_streams_data_run_mtg20190222_rfile.R
@@ -22,7 +22,7 @@
 #call in packages
 
 library(tidyverse)
-
+library(lubridate)
 
 #set working directory
 
@@ -30,16 +30,59 @@ setwd('C:/Users/etaylor21/Documents/gnv_streams')
 
 #call in data file
 
-data=read_csv('gnv_streams_data_run_mtg20190222.csv')
+data_nut=read_csv('gnv_streams_data_run_mtg20190222.csv', col_types = cols(
+  Site = col_character(),
+  Date = col_date(format = "%m/%d/%Y"),
+  Time = col_time(format = ""),
+  REP = col_character(),
+  NH4 = col_double(),
+  NOx = col_double(),
+  OP = col_double())); problems(data_nut)#nutrient data; fixed date format for date column
 
-#reshape data to organize
+data_nut$Date#double checked date converted correctly
+  
+data_fp=read_csv('gnv_streams_fieldparam_data_run_mtg20190222.csv', col_types = cols(
+  Site = col_character(),
+  Date = col_date(format = "%m/%d/%Y"),
+  Time = col_time(format = ""),
+  DO = col_double(),
+  DO_Sat = col_double(),
+  pH = col_double(),
+  SpCond = col_double(),
+  Temp = col_double(),
+  Turb = col_double())); problems(data_fp)#field parameter data
 
-data_spread=spread(data = data, key = 'Parameter', value = 'Result')
-as.data.frame(data_spread)
+#change date to show only as month; will need to change as POR spans > than 1 year
 
-#create new csv with spread data
+lubridate::floor_date(data_nut$Date, unit = 'month')
+lubridate::floor_date(data_fp$Date, unit = 'month')
 
-write_excel_csv(data_spread, path = 'C:/Users/etaylor21/Documents/gnv_streams/gnv_streams_data_run_mtg20190222.csv', 
+#reshaping data so all units read in a column
+
+data_nut_2=gather(data_nut, 'NH4', 'NOx', 'OP', key = 'Analyte', value = "Result")
+
+write_excel_csv(data_nut_2, path = 'C:/Users/etaylor21/Documents/gnv_streams/gnv_streams_data_run_mtg20190222.csv', 
                 na = 'NA', append =FALSE, col_names = TRUE)
 
+data_nut_2
+
+
+data_fp_2=gather(data_fp, 'DO', 'DO_Sat', 'pH', 'SpCond', 'Temp', 'Turb', key = 'Analyte', value = "Result")
+
+write_excel_csv(data_fp_2, path = 'C:/Users/etaylor21/Documents/gnv_streams/gnv_streams_data_run_mtg20190222_v2.csv', 
+                na = 'NA', append =FALSE, col_names = TRUE)
+
+data_fp_2
+
+#read the nutrient REPs as averages instead of 3 individual points on a sampling trip
+
+group_data_nut = group_by(data_nut_2, Site, Date, Analyte)#by which columns am I grouping my data by
+group_data_nut#check the groups are correct
+data_nut_USE_ME = summarise(group_data_nut, mean = mean(Result))#calculate average of result for analyte test
+
+
+
+f = ggplot(data_nut_USE_ME, aes(x=Date, y=mean, group = Site, color = Analyte, shape = Site)) 
+
+f + geom_point()
 
