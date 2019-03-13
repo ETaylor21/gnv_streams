8bcba962e115501751c385426d19f492284ff74b
ETaylor21 <etaylor21@ufl.edu>
2019-03-13 19:14:09 -0400

    updated code and figures for CLCE Poster

--- CLCE_poster_gnvstrm.R
+++ CLCE_poster_gnvstrm.R
@@ -31,36 +31,159 @@ setwd('C:/Users/etaylor21/Documents/gnv_streams')
 
 #call in field parameter data file
 
-data.nut=read_csv('gnv_streams_data_run_mtg20190222.csv')
+data_nut=read_csv('gnv_streams_data_run_mtg20190222.csv')
 
-data.fp=read_csv('gnv_streams_data_run_mtg20190222_v2.csv')
+data_fp=read_csv('gnv_streams_data_run_mtg20190222_v2.csv')
 
-data.fp = drop_na(data.fp)
+data_fp = drop_na(data_fp)
 
-data.nut
-data.fp
+data_nut
+data_fp
 
-############grouping data for nutrient figures#############
+############subsetting data to be not include September 2018 samplng date############
 
-group.data.nut = group_by(data.nut, Site, Analyte)#by which columns am I grouping my data by
-group.data.nut
-summarise(group.data.nut)
+#Drop the data from Sept
 
-data.nut.bar.USE.ME = summarise(group.data.nut, mean = mean(Result), std.error = sqrt(var(Result)/length(Result)))#calculate average of result for analyte test and standard error
-data.nut.bar.USE.ME
+nd = data_nut %>% 
+      filter(Date >= as.Date('2018-10-18') & Date <=  as.Date('2019-02-06')) %>%
+      group_by(Site, Analyte) %>%
+      summarise(mean = mean(Result), std.error = sqrt(var(Result)/length(Result)))#calculate average of result for analyte test and standard error
+
+nd
 
 #####Nutrient Bar Chart plotting############
 
-nutrient = ggplot(data.nut.bar.USE.ME, aes(x = Site, y = mean, fill = Analyte))
+nutrient = ggplot(nd, aes(x = Site, y = mean, fill = Analyte))
 
 p = nutrient + geom_col(position = 'dodge') + ylab('Results (mg/L)') #make column chart
 
 pf = p + geom_errorbar(aes(ymin = mean-std.error, ymax = mean+std.error), width = 0.2, position = position_dodge(0.9)) + 
   facet_wrap( . ~ Analyte , scales = 'free_y', nrow = 3, 
-              labeller = as_labeller(c(NH4 = "Ammonium (N)", NOx = "Nitrate-Nitrite (N)", OP = 'Orthophosphate (P)')))#adding error bars to column chart and creating facets; 
+              labeller = as_labeller(c(NH4 = "Ammonium (N)", 
+                                       NOx = "Nitrate-Nitrite (N)", 
+                                       OP = 'Orthophosphate (P)')))#adding error bars to column chart and creating facets; 
   #in facet wrap the first changes whether panels are arranged horizantally or vertically, second allows sites to be different on the y, 
   #third changes the amont of rows to 3 and columns to 0 so that figures stack and headers stay on top
 
 pf + scale_fill_manual(values = c('#56B4E9','#D55E00', '#009E73' )) + guides(fill = FALSE)#changes colors on bar fill to colorblind friendly
 
+###############Nutrient Box Plot (nbp)################
+nd2 = data_nut %>% 
+  filter(Date >= as.Date('2018-10-18') & Date <=  as.Date('2019-02-06')) %>%
+  group_by(Site, Analyte)
+
+nd2
+
+npb = ggplot(nd2, aes(x = Site, y = Result, fill = Analyte)) +
+      geom_boxplot() + ylab('Results (mg/L)')
+
+npb + scale_fill_manual(values = c('#56B4E9','#D55E00', '#009E73' ))
+
+#faceted boplots
+
+npb2 = npb + facet_wrap( . ~ Analyte , scales = 'free_y', nrow = 3, 
+                         labeller = as_labeller(c(NH4 = "Ammonium (N)", 
+                                                  NOx = "Nitrate-Nitrite (N)", 
+                                                  OP = 'Orthophosphate (P)'))) +
+  scale_fill_manual(values = c('#56B4E9','#D55E00', '#009E73' )) + guides(fill = FALSE)
+
+
+npb2
+
+#####Nutrient Time-Series (nts)#####
+
+nut_ts = ggplot(nd2, aes(x = Date, y = Result, fill = Site))
+
+nts = nut_ts + geom_line(aes(group = Site, color = Site)) + ylab('Results (mg/L)') + 
+  geom_point(size = 2, aes(x=Date, color = Site, shape = Site)) + 
+  facet_wrap( . ~ Analyte , scales = 'free_y', nrow = 3, 
+                            labeller = as_labeller(c(NH4 = "Ammonium (N)", 
+                                       NOx = "Nitrate-Nitrite (N)", 
+                                       OP = 'Orthophosphate (P)'))) +
+  scale_color_brewer(palette = 'Dark2') + guides(fill = FALSE)
+
+nts
+
+#####Nutrient Data of just Possum Creek and Hogtown Upstream (pchu_ts)#####
+
+pchu = data_nut %>% 
+  filter(Date >= as.Date('2018-10-18') & Date <=  as.Date('2019-02-06') ) %>%
+  filter(Site == 'HOGNW16' | Site == 'POSNW16') %>%
+  group_by(Date, Site, Analyte) %>%
+  summarise(mean = mean(Result))
+
+
+pchu
+
+pchu_nut_ts = ggplot(pchu, aes(x = Date, y = mean, fill = Site))
+
+pchu_ts = pchu_nut_ts + geom_line(aes(group = Site, color = Site)) + ylab('Results (mg/L)') + 
+  geom_point(size = 2, aes(x=Date, color = Site, shape = Site)) + 
+  facet_wrap( . ~ Analyte , scales = 'free_y', nrow = 3, 
+                            labeller = as_labeller(c(NH4 = "Ammonium (N)", 
+                                       NOx = "Nitrate-Nitrite (N)", 
+                                       OP = 'Orthophosphate (P)'))) +
+  scale_color_brewer(palette = 'Dark2') + guides(fill = FALSE)
+
+pchu_ts
+
+
+
+############################################################################################################
+
+##############Field Parameter Bar Charts############
+
+fp = data_fp %>% 
+  filter(Date >= as.Date('2018-10-18') & Date <=  as.Date('2019-02-06') ) %>%
+              filter(Analyte == 'DO_Sat' | Analyte == 'pH' | Analyte == 'SpCond' | Analyte == 'Temp' | Analyte == 'Turb') %>%
+              group_by(Site, Analyte) %>% 
+              summarise(mean = mean(Result), std.error = sqrt(var(Result)/length(Result)))#calculate average of result for analyte test and standard error
+
+fp
+
+fp2 = data_fp %>% 
+  filter(Date >= as.Date('2018-10-18') & Date <=  as.Date('2019-02-06') ) %>%
+  filter(Analyte == 'DO_Sat' | Analyte == 'pH' | Analyte == 'SpCond' | Analyte == 'Temp' | Analyte == 'Turb') %>%
+  group_by(Site, Analyte)
+
+fp2
+
+#####Field Parameter Bar Chart (fpb)############
+
+field_param = ggplot(fp, aes(x = Site, y = mean, fill = Analyte))
+
+field_param_bar = field_param + geom_col(position = 'dodge') + ylab(NULL) #make column chart
+
+fpb = field_param_bar + geom_errorbar(aes(ymin = mean-std.error, ymax = mean+std.error), width = 0.2, position = position_dodge(0.9)) + 
+  facet_wrap( . ~ Analyte , scales = 'free_y', nrow = 5, 
+              strip.position = 'left',
+              labeller = as_labeller(c(DO_Sat = "DO Saturation (%)", 
+                                       pH = 'pH (SU)', 
+                                       SpCond = 'SpCond (uS/cm)', 
+                                       Temp = 'Temperature (degC)', 
+                                       Turb = 'Turbidity (NTU)' ))) + 
+                theme(strip.placement = 'outside')#adding error bars to column chart and creating facets; 
+#in facet wrap the first changes whether panels are arranged horizantally or vertically, second allows sites to be different on the y, 
+#third changes the amont of rows to 3 and columns to 0 so that figures stack and headers stay on top
+
+fpb + scale_fill_manual(values = c('#56B4E9','#D55E00', '#009E73', '#CC79A7', '#0072B2' )) + guides(fill = FALSE)#changes colors on bar fill to colorblind friendly
+
+#####Field Parameter Time Series (fpts)#####
+
+field_param_ts = ggplot(fp2, aes(x = Date, y = Result))
+
+fpts = field_param_ts + geom_line(aes(group = Site, color = Site)) + ylab(NULL) + 
+  geom_point(size = 2, aes(x=Date, color = Site, shape = Site)) + 
+  facet_wrap( . ~ Analyte , scales = 'free_y', nrow = 5, 
+              strip.position = 'left',
+              labeller = as_labeller(c(DO_Sat = "DO Saturation (%)", 
+                                       pH = 'pH (SU)', 
+                                       SpCond = 'SpCond (uS/cm)', 
+                                       Temp = 'Temperature (degC)', 
+                                       Turb = 'Turbidity (NTU)' ))) + 
+  theme(strip.placement = 'outside')#adding error bars to column chart and creating facets; 
+#in facet wrap the first changes whether panels are arranged horizantally or vertically, second allows sites to be different on the y, 
+#third changes the amont of rows to 3 and columns to 0 so that figures stack and headers stay on top
+
+fpts + scale_fill_manual(values = c('#56B4E9','#D55E00', '#009E73', '#CC79A7', '#0072B2' )) + guides(fill = FALSE)
 
